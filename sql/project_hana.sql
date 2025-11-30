3fbdfb2af46897df504dbefe7337d10e9ce46db6
hanadwlle02 <hanadwlle01@gmail.com>
2025-11-30 11:24:57 +0800

    Menambahkan project awal

--- sql/project_hana.sql
+++ sql/project_hana.sql
@@ -1,14 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2025 at 04:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-CREATE DATABASE IF NOT EXISTS project_hana;
-USE project_hana;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE produk (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(100) NOT NULL,
-    kategori VARCHAR(50) NOT NULL,
-    harga DECIMAL(10,2) NOT NULL,
-    stok INT NOT NULL,
-    gambar VARCHAR(255) DEFAULT NULL,
-    status ENUM('ready','sold') NOT NULL DEFAULT 'ready',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project_hana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `status` enum('ready','sold') NOT NULL DEFAULT 'ready'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id`, `nama`, `kategori`, `harga`, `stok`, `gambar`, `status`) VALUES
+(1, 'sanggul bali', 'Fashion', 100000.00, 50, '1764463908_sanggulbali.png', 'ready'),
+(2, 'kebaya', 'Fashion', 170000.00, 15, '1764465936_kebaya.png', 'ready'),
+(3, 'heals wanita', 'Fashion', 200000.00, 25, '1764469164_sandal.png', 'ready');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
